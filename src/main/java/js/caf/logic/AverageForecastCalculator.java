85530ad0a42ea0c67683e1b08537be20fa023de1
marco.saxer <marco.saxer@CPX-XA89L2HA6H6.dir.svc.accenture.com>
2016-11-22 17:21:58 +0100

    implements the logic, tests are green

--- src/main/java/js/caf/logic/AverageForecastCalculator.java
+++ src/main/java/js/caf/logic/AverageForecastCalculator.java
@@ -1,16 +1,29 @@
 package js.caf.logic;
 
-import js.caf.domain.DailyForecast;
-
 import java.util.List;
 
-/**
- * Created by mentlsve on 22/11/16.
- */
+import js.caf.domain.DailyForecast;
+import js.caf.exception.NotEnoughDataPointsException;
+
+/** 
+* Created by mentlsve on 22/11/16. 
+*/
 public class AverageForecastCalculator {
 
-    public float calculateAverageMaxTemperatureOverDays(List<DailyForecast> dailyForecasts, int numberOfDays){
-        return 0.0f;
-    }
+	public float calculateAverageMaxTemperatureOverDays(List<DailyForecast> dailyForecasts, int numberOfDays) {
+		if (dailyForecasts.size() <= 2) {
+			throw new NotEnoughDataPointsException();
+		}
+
+		float average = 0.0f;
+
+		int min = Math.min(dailyForecasts.size(), numberOfDays);
+		for (int i = 0; i < min; i++) {
+			average += dailyForecasts.get(i).getMaxTemperatureInCelsius();
+		}
+		average /= min;
+
+		return average;
+	}
 
 }
